7ea219873307e4b1d027f9161759239d5cf94cc9
Merc5Dark <31131794siya@gmail.com>
2023-08-22 16:42:36 +0200

    Heading into React Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import { Component } from "react";
-
-class App extends Component {
-  render() {
-    return <h1>React Components</h1>
-  }
+function App () {
+  return <h1>React Components</h1>
 }
 
 export default App;
